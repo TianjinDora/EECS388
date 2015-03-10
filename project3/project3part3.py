1e6c5c189c496b080e056f7c540e7560d41fb944
nfralick <nfralick@umich.edu>
2015-03-10 08:07:12 -0400

    Part 3, not correct yet

---
+++ project3/project3part3.py
@@ -0,0 +1,38 @@
+import dpkt
+import sys
+
+f = open('C:\Users\\nathan\Downloads\lbl-internal.20041004-1305.port002.dump.anon', "rb")
+pcap = dpkt.pcap.Reader(f)
+
+ipCounts = {}
+
+for ts, buf in pcap:
+    try:
+        eth = dpkt.ethernet.Ethernet(buf)
+        if eth.type != dpkt.ethernet.ETH_TYPE_IP: continue # if not IP packet, continue
+        ip = eth.data
+
+        # only process TCP packets
+        if ip.p == dpkt.ip.IP_PROTO_TCP:
+            packet = ip.data
+
+
+            if packet.flags & dpkt.tcp.TH_SYN != 0: # if it's a SYN packet
+                # initialize in dictionary if it doesn't exist
+                if ip.src not in ipCounts: ipCounts[ip.src] = {'SYN': 0, 'SYNACK': 0}
+                
+                if packet.flags & dpkt.tcp.TH_ACK != 0: # if it's an ACK packet too
+                    ipCounts[ip.src]['SYNACK'] += 1
+                else:
+                    ipCounts[ip.src]['SYN'] += 1
+                
+    except dpkt.UnpackError:
+        print: "not an ethernet packet"
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+
+print ipCounts
+
+for item in ipCounts.items():
+	if item[1]['SYN'] > item[1]['SYNACK'] * 3:
+		print item[0]
