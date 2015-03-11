dc7e4f667e003f73ae0a65c627f0a9515b299252
nfralick <nfralick@umich.edu>
2015-03-11 12:18:01 -0400

    oops they send syns but receive synacks

--- project3/project3part3.py
+++ project3/project3part3.py
@@ -9,29 +9,30 @@ ipCounts = {}
 for ts, buf in pcap:
     try:
         eth = dpkt.ethernet.Ethernet(buf)
-        if eth.type != dpkt.ethernet.ETH_TYPE_IP: continue # if not IP packet, continue
-        ip = eth.data
-
-        # only process TCP packets
-        if ip.p == dpkt.ip.IP_PROTO_TCP:
-            packet = ip.data
-
-
-            if packet.flags & dpkt.tcp.TH_SYN != 0: # if it's a SYN packet
-                # initialize in dictionary if it doesn't exist
-                if ip.src not in ipCounts: ipCounts[ip.src] = {'SYN': 0, 'SYNACK': 0}
-                
-                if packet.flags & dpkt.tcp.TH_ACK != 0: # if it's an ACK packet too
-                    ipCounts[ip.src]['SYNACK'] += 1
-                else:
-                    ipCounts[ip.src]['SYN'] += 1
-                
     except dpkt.UnpackError:
-        print: "not an ethernet packet"
-    except:
-        print "Unexpected error:", sys.exc_info()[0]
+        print "not an ethernet packet"
+        continue
+        
+    if eth.type != dpkt.ethernet.ETH_TYPE_IP: continue # if not IP packet, continue
+    ip = eth.data
+
+    # only process TCP packets
+    if ip.p == dpkt.ip.IP_PROTO_TCP:
+        packet = ip.data
+
+
+        if packet.flags & dpkt.tcp.TH_SYN != 0: # if it's a SYN packet
+            # initialize in dictionary if it doesn't exist
+            if ip.src not in ipCounts: ipCounts[ip.src] = {'SYN': 0, 'SYNACK': 0}
+            
+            if packet.flags & dpkt.tcp.TH_ACK != 0: # if it's an ACK packet too
+                ipCounts[ip.src]['SYNACK'] += 1
+            else:
+                ipCounts[ip.src]['SYN'] += 1
 
 print ipCounts
+
+for item in ipCounts.teims(): print item
 
 for item in ipCounts.items():
 	if item[1]['SYN'] > item[1]['SYNACK'] * 3:
